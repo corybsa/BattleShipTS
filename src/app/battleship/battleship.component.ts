955c65226c77a16096a3b2064920c9a524ed243f
haruku <corybsa@gmail.com>
2018-10-10 14:29:32 -0400

    Change css.

--- src/app/battleship/battleship.component.ts
+++ src/app/battleship/battleship.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import {AfterContentInit, Component, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'castle-battleship',
   styleUrls: ['./battleship.component.css'],
   templateUrl: './battleship.component.html'
 })
-export class BattleshipComponent {
+export class BattleshipComponent implements AfterContentInit {
+  dimensions = 10;
+
+  @ViewChild('aiBoard') aiBoard: HTMLTableElement;
+  @ViewChild('playerBoard') playerBoard: HTMLTableElement;
+
   constructor() {
 
+  }
+
+  ngAfterContentInit() {
+    this.generateBoard(this.aiBoard);
+  }
+
+  generateBoard(board: HTMLTableElement) {
+    console.log(board);
   }
 }
