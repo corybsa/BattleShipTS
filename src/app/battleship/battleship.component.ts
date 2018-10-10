253b4e05aab0ed80ef7b6abcada4b2a338d5c7ee
haruku <corybsa@gmail.com>
2018-10-10 15:56:37 -0400

    Row and column highlight.

--- src/app/battleship/battleship.component.ts
+++ src/app/battleship/battleship.component.ts
@@ -1,4 +1,4 @@
-import {AfterContentInit, Component, OnInit, ViewChild} from '@angular/core';
+import {AfterContentInit, Component, ContentChild, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'castle-battleship',
@@ -9,18 +9,26 @@ export class BattleshipComponent implements AfterContentInit {
   dimensions = 10;
   boardRows = [];
 
-  @ViewChild('aiBoard') aiBoard: HTMLTableElement;
-  @ViewChild('playerBoard') playerBoard: HTMLTableElement;
+  @ViewChild('aiBoard') aiBoard: ElementRef;
+  @ViewChild('playerBoard') playerBoard: ElementRef;
+  @ViewChild('aiSideCoordinates') aiSideCoordinates: ElementRef;
+  @ViewChild('aiTopCoordinates') aiTopCoordinates: ElementRef;
 
   constructor() {
     this.boardRows = Array(this.dimensions).fill(0).map((x, i) => i);
   }
 
   ngAfterContentInit() {
-    this.generateBoard(this.aiBoard);
+
   }
 
-  generateBoard(board: HTMLTableElement) {
-    console.log(board);
+  highlight(row, col) {
+    this.aiSideCoordinates.nativeElement.childNodes[row].classList.add('highlight');
+    this.aiTopCoordinates.nativeElement.childNodes[col].classList.add('highlight');
+  }
+
+  unhighlight(row, col) {
+    this.aiSideCoordinates.nativeElement.childNodes[row].classList.remove('highlight');
+    this.aiTopCoordinates.nativeElement.childNodes[col].classList.remove('highlight');
   }
 }
