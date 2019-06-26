8d3fc2aba7d10542d8c5105aae5e1128b34515ab
Cory Sandlin <corybsa@leeschools.net>
2019-06-26 14:33:06 -0400

    Remove battleship folder.

--- src/app/battleship/classes/player-type.model.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @property PLAYER number: Represents the player.
- * @property AI number: Represents the AI.
- */
-export enum PlayerType {
-  PLAYER = 0,
-  AI
-}
--- src/tests/ai.spec.ts
+++ src/tests/ai.spec.ts
@@ -1,6 +1,8 @@
 import { async, TestBed } from '@angular/core/testing';
 import { AI } from '../app/classes/ai.model';
 import { AppComponent } from '../app/app.component';
+import { HitType } from '../app/classes/hit-info.model';
+import { Orientation } from '../app/classes/orientation.model';
 
 describe('AI', () => {
   beforeEach(async(() => {
@@ -15,6 +17,18 @@ describe('AI', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
 
-    expect(app.ai).toBeTruthy();
+    app.ai.memory = [{
+      coordinates: [{col: 1, row: 1}],
+      lastAttack: {shipId: 0, hitType: HitType.HIT},
+      orientation: Orientation.DOWN,
+      shipId: 0
+    }];
+
+    expect(app.ai.memory).toEqual([{
+      coordinates: [{col: 1, row: 1}],
+      lastAttack: {shipId: 0, hitType: HitType.HIT},
+      orientation: Orientation.DOWN,
+      shipId: 0
+    }]);
   });
 });
